7d352707db3d75ec0cc54d517fe4124894b0f270
Judah3191n <W0509673@nscc.ca>
2025-01-08 11:51:46 -0400

    Exercise 1 final

--- Exercise 1/script.js
+++ Exercise 1/script.js
@@ -1,8 +1,1 @@
 console.log("This is my first JavaScript script!");
-document.write("Hello guys");
-
-
-    for (var i = 0;i<1000;i++){
-        document.querySelector('h1').style.color='green';
-        document.querySelector('h1').style.color='red';
-    }
