4ad97fe7092fcd60a2d873dc9636037958cf8883
Judah3191n <W0509673@nscc.ca>
2025-01-08 08:42:35 -0400

    Exercise 1
    Exercise with part of in class activity

--- Exercise 1/script.js
+++ Exercise 1/script.js
@@ -1,1 +1,8 @@
 console.log("This is my first JavaScript script!");
+document.write("Hello guys");
+
+
+    for (var i = 0;i<1000;i++){
+        document.querySelector('h1').style.color='green';
+        document.querySelector('h1').style.color='red';
+    }
